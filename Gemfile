26cab84cd0b4abc14fcebf6ba40941f56626a875
Dmitry Sokurenko <dmitry@sokurenko.name>
2015-12-01 18:51:37 +0300

    remove slim and handlebars gems

--- Gemfile
+++ Gemfile
@@ -4,9 +4,6 @@ gem "middleman"
 gem 'middleman-jasmine'
 gem 'middleman-react' # allows JSX in JS files, react.js is included through bower
 gem 'sprockets-coffee-react'
-gem 'handlebars_assets' # precompiles HB templates, handlebars.js can be included through bower
-
-gem 'slim'
 gem 'font-awesome-sass'
 
 gem 'snapshot', '0.10.2', require: false
--- config.rb
+++ config.rb
@@ -1,14 +1,8 @@
-require 'slim'
-require 'handlebars_assets'
 require 'sprockets/coffee-react'
-
-::Slim::Engine.disable_option_validator!
-
 
 ::Sprockets.register_preprocessor 'application/javascript', ::Sprockets::CoffeeReact
 ::Sprockets.register_engine '.cjsx', ::Sprockets::CoffeeReactScript
 ::Sprockets.register_engine '.js.cjsx', ::Sprockets::CoffeeReactScript
-
 
 page "index.html", :layout => false
 
@@ -16,39 +10,22 @@ activate :react do |config|
   config.harmony = true
 end
 
-configure :development do
-  activate :jasmine
-end
-
 set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'images'
 set :build_dir, 'www'
+
+configure :development do
+  activate :jasmine
+end
 
 configure :build do
   # activate :minify_css
   # activate :minify_javascript
   # activate :asset_hash
   # activate :relative_assets
-  # set :http_prefix, "/Content/images/"
 end
 
 after_configuration do
   sprockets.append_path File.join root, 'bower_components'
-  sprockets.append_path HandlebarsAssets.path
-end
-
-ready do
-end
-
-helpers do
-  def js_page(page_id, options = {}, &block)
-    @classes = options[:classes]
-    @page_id = page_id
-    partial :page, locals: { pid: page_id }, &block
-  end
-
-  def navbar(&block)
-    content_tag :ul, class: "navbar for-#{@page_id}", &block
-  end
 end
